6fc16e0b3fded18f2d8864c34975d104428b94ad
sergeyiksanov <sergeyiksanov25@gmail.com>
2025-04-14 23:55:11 +0300

    added Message model

---
+++ app/src/main/java/com/al/firebaseexample/Message.java
@@ -0,0 +1,13 @@
+package com.al.firebaseexample;
+
+public class Message {
+    public String content;
+    public String author;
+
+    public Message() {}
+
+    public Message(String content, String author) {
+        this.content = content;
+        this.author = author;
+    }
+}
